a2045c1d53113d947dde445d41dfb2ccc68ef7a3
Fahrizal Aldi <121169138+Rhesheyz@users.noreply.github.com>
2025-04-17 11:27:58 +0700

    Update settings.py

--- lomba_ecotourism/settings.py
+++ lomba_ecotourism/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
-ALLOWED_HOSTS = "127.0.0.1", "localhost"
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 # Application definition
